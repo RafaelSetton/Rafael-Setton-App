6476bbf8a92e88a7ff0ac1e56efce342444f293c
Rafael Setton <rasealca2017@gmail.com>
2021-07-20 10:09:32 -0400

    Update Calculadora

--- lib/pages/calculadora/index.dart
+++ lib/pages/calculadora/index.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
-
 import 'package:flutter/rendering.dart';
+import 'package:petitparser/petitparser.dart';
 
 class CalculadoraPage extends StatefulWidget {
   @override
@@ -13,26 +13,11 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
   int maxScreenLength = 24;
   String? varA, varB, varC, varD;
 
-  void click(num) {
+  void click(dynamic n) {
     setState(() {
-      String x = num.toString();
-      if (calculus.length < maxScreenLength) {
-        try {
-          if ('^X÷+-('.contains(calculus[-2]) && '^X÷+-'.contains(x[1])) {
-            calculus = calculus.substring(0, calculus.length - 3) + x;
-          } else if (calculus.length > 0 || !('^X÷+-'.contains(x[1]))) {
-            calculus += x;
-          }
-        } catch (err) {
-          try {
-            if (calculus.length > 0 || !('^X÷+-'.contains(x[1]))) {
-              calculus += x;
-            }
-          } catch (err2) {
-            calculus += x;
-          }
-        }
-      }
+      String x = n.toString();
+      if (calculus.length < maxScreenLength) calculus += x;
+
       while (calculus.startsWith('0') && calculus.length != 1) {
         calculus = calculus.substring(1);
       }
@@ -40,23 +25,21 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
   }
 
   void virgula() {
+    String ops = 'πe+-X÷^()';
+    for (int i = calculus.length - 1; i >= 0; i--) {
+      String c = calculus[i];
+      if (ops.contains(c))
+        break;
+      else if (c == ',') return;
+    }
     setState(() {
-      List<String> lista = calculus.split(' ');
-      if (!(lista[lista.length - 1].contains(','))) {
-        calculus += ',';
-      }
+      calculus += ',';
     });
   }
 
   void delete() {
     setState(() {
-      int ult;
-      if (calculus[calculus.length - 1] == ' ') {
-        ult = 3;
-      } else {
-        ult = 1;
-      }
-      calculus = calculus.substring(0, calculus.length - ult);
+      calculus = calculus.substring(0, calculus.length - 1);
     });
   }
 
@@ -66,132 +49,42 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
     });
   }
 
+  Parser buildParser() {
+    final builder = ExpressionBuilder();
+    builder.group()
+      ..primitive((pattern('+-').optional() &
+              digit().plus() &
+              (char(',') & digit().plus()).optional() &
+              (pattern('eE') & pattern('+-').optional() & digit().plus())
+                  .optional())
+          .flatten('number expected')
+          .trim()
+          .map(num.tryParse))
+      ..wrapper(
+          char('(').trim(), char(')').trim(), (left, value, right) => value);
+    builder.group().prefix(char('-').trim(), (op, num a) => -a);
+    builder
+        .group()
+        .right(char('^').trim(), (num a, op, num b) => math.pow(a, b));
+    builder.group()
+      ..left(char('X').trim(), (num a, op, num b) => a * b)
+      ..left(char('÷').trim(), (num a, op, num b) => a / b);
+    builder.group()
+      ..left(char('+').trim(), (num a, op, num b) => a + b)
+      ..left(char('-').trim(), (num a, op, num b) => a - b);
+    return builder.build().end();
+  }
+
   String calc(String string) {
-    void syntaxError() {
-      setState(() {
-        calculus = "Syntax Error";
-      });
-    }
-
-    bool validacao(List<String> str) {
-      try {
-        double.parse(str[0]);
-      } catch (err) {
-        if (!'(eπ'.contains(str[0])) {
-          syntaxError();
-          return false;
-        }
-      }
-
-      int cont = 0;
-      for (var char in str) {
-        if (char == '(') {
-          cont++;
-        } else if (char == ')') {
-          cont--;
-        }
-        if (cont < 0) {
-          syntaxError();
-          return false;
-        }
-      }
-      if (cont != 0) {
-        syntaxError();
-        return false;
-      }
-      return true;
-    }
-
-    List<int> findParenthesis(List<String> str) {
-      str.removeWhere((x) => x.length == 0);
-      int start = str.indexOf('(') + 1;
-      int back, front;
-      while (true) {
-        back = str.indexOf(')', start);
-        front = str.indexOf('(', start);
-        if (front < 0 || back < front) {
-          return [start, back];
-        }
-        start = front + 1;
-      }
-    }
-
-    string = string.replaceAll(',', '.');
     string = string.replaceAll('π', math.pi.toString());
     string = string.replaceAll('e', math.e.toString());
-    string = string.replaceAll('  ', ' ');
-    if (string.startsWith(' ')) {
-      string = string.substring(1);
-    }
-    if (string.endsWith(' ')) {
-      string = string.substring(0, string.length - 1);
-    }
-    List<String> listString = string.split(' ');
-    validacao(listString);
 
-    while (listString.contains('(')) {
-      int ini = findParenthesis(listString)[0];
-      int fim = findParenthesis(listString)[1];
-      List<String> novo = listString.sublist(ini, fim);
-      for (var i = 0; i < fim - ini + 2; i++) {
-        listString.removeAt(ini - 1);
-      }
-      String insert = calc(novo.join(" ")).replaceAll(',', '.');
-      listString.insert(ini - 1, insert);
-    }
-
-    int i = 1;
-    while (listString.contains('^') && i < listString.length) {
-      if (listString[i] == '^') {
-        double f1 = double.parse(listString.removeAt(i - 1));
-        listString.removeAt(i - 1);
-        double f2 = double.parse(listString.removeAt(i - 1));
-        listString.insert(i - 1, math.pow(f1, f2).toString());
-        i--;
-      }
-      i++;
-    }
-
-    i = 1;
-    while ((listString.contains('X') || listString.contains('÷')) &&
-        i < listString.length) {
-      if (listString[i] == 'X') {
-        double f1 = double.parse(listString.removeAt(i - 1));
-        listString.removeAt(i - 1);
-        double f2 = double.parse(listString.removeAt(i - 1));
-        listString.insert(i - 1, (f1 * f2).toString());
-        i--;
-      } else if (listString[i] == '÷') {
-        double f1 = double.parse(listString.removeAt(i - 1));
-        listString.removeAt(i - 1);
-        double f2 = double.parse(listString.removeAt(i - 1));
-        listString.insert(i - 1, (f1 / f2).toString());
-        i--;
-      }
-      i++;
-    }
-
-    i = 1;
-    while ((listString.contains('+') || listString.contains('-')) &&
-        i < listString.length) {
-      if (listString[i] == '+') {
-        double f1 = double.parse(listString.removeAt(i - 1));
-        listString.removeAt(i - 1);
-        double f2 = double.parse(listString.removeAt(i - 1));
-        listString.insert(i - 1, (f1 + f2).toString());
-        i--;
-      } else if (listString[i] == '-') {
-        double f1 = double.parse(listString.removeAt(i - 1));
-        listString.removeAt(i - 1);
-        double f2 = double.parse(listString.removeAt(i - 1));
-        listString.insert(i - 1, (f1 - f2).toString());
-        i--;
-      }
-      i++;
-    }
-
-    string = listString[0].replaceAll('.', ',');
-    return string;
+    final parser = buildParser();
+    final result = parser.parse(string);
+    if (result.isSuccess)
+      return result.value;
+    else
+      return result.message;
   }
 
   void a() {
@@ -234,9 +127,8 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
     });
   }
 
-  Container myContainer(
-      {required String text,
-      double width = 0.25,
+  Container myContainer(String text,
+      {double width = 0.25,
       Color bg = Colors.white70,
       void Function()? callback}) {
     if (callback == null) {
@@ -264,16 +156,18 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
 
   @override
   Widget build(BuildContext context) {
+    AppBar appBar = AppBar(
+      title: Text("Calculadora"),
+      centerTitle: true,
+      leading: BackButton(),
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora"),
-        centerTitle: true,
-        leading: BackButton(),
-      ),
+      appBar: appBar,
       body: Column(
         children: <Widget>[
           Container(
-            height: MediaQuery.of(context).size.height / 5.75,
+            height: MediaQuery.of(context).size.height * 0.3 -
+                appBar.preferredSize.height,
             padding: EdgeInsets.all(10.0),
             alignment: Alignment.center,
             color: Colors.black,
@@ -289,69 +183,59 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "CE", bg: Colors.orange, callback: clear),
-              myContainer(text: "⌫", bg: Colors.green, callback: delete),
-              myContainer(text: " ( ", bg: Colors.lightBlueAccent),
-              myContainer(text: " ) ", bg: Colors.lightBlueAccent),
+              myContainer("CE", bg: Colors.orange, callback: clear),
+              myContainer("⌫", bg: Colors.green, callback: delete),
+              myContainer("(", bg: Colors.lightBlueAccent),
+              myContainer(")", bg: Colors.lightBlueAccent),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: " π ", bg: Colors.green),
-              myContainer(text: " e ", bg: Colors.green),
-              myContainer(
-                  text: "xʸ",
-                  bg: Colors.green,
-                  callback: () {
-                    click(" ^ ");
-                  }),
-              myContainer(text: " ÷ ", bg: Colors.green),
+              myContainer("π", bg: Colors.green),
+              myContainer("e", bg: Colors.green),
+              myContainer("xʸ", bg: Colors.green, callback: () => click("^")),
+              myContainer("÷", bg: Colors.green),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "1"),
-              myContainer(text: "2"),
-              myContainer(text: "3"),
-              myContainer(text: " X ", bg: Colors.green),
+              myContainer("1"),
+              myContainer("2"),
+              myContainer("3"),
+              myContainer("X", bg: Colors.green),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "4"),
-              myContainer(text: "5"),
-              myContainer(text: "6"),
-              myContainer(text: " - ", bg: Colors.green),
+              myContainer("4"),
+              myContainer("5"),
+              myContainer("6"),
+              myContainer("-", bg: Colors.green),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "7"),
-              myContainer(text: "8"),
-              myContainer(text: "9"),
-              myContainer(text: " + ", bg: Colors.green),
+              myContainer("7"),
+              myContainer("8"),
+              myContainer("9"),
+              myContainer("+", bg: Colors.green),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "0", width: 0.5),
-              myContainer(text: ",", bg: Colors.white54, callback: virgula),
-              myContainer(
-                  text: "=",
-                  bg: Colors.green,
-                  callback: () {
-                    setState(() {
-                      calculus = calc(calculus);
-                    });
-                  }),
+              myContainer("0", width: 0.5),
+              myContainer(",", bg: Colors.white54, callback: virgula),
+              myContainer("=", bg: Colors.green, callback: () {
+                setState(() => calculus = calc(calculus));
+              }),
             ],
           ),
           Row(
             children: <Widget>[
-              myContainer(text: "A", bg: Colors.red, callback: a),
-              myContainer(text: "B", bg: Colors.red, callback: b),
-              myContainer(text: "C", bg: Colors.red, callback: c),
-              myContainer(text: "D", bg: Colors.red, callback: d),
+              myContainer("A", bg: Colors.red, callback: a),
+              myContainer("B", bg: Colors.red, callback: b),
+              myContainer("C", bg: Colors.red, callback: c),
+              myContainer("D", bg: Colors.red, callback: d),
             ],
           ),
         ],
